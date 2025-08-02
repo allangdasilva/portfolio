e5f484cecf815726cee4fa32cb311f026e0816b1
Allan Silva <allandasilva33@outlook.com>
2025-08-02 17:56:23 -0300

    Implementação da Hero Section concluída

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,37 +4,68 @@ import Image from "next/image";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
 import { useGSAP } from "@gsap/react";
+import React from "react";
 gsap.registerPlugin(SplitText, useGSAP);
 
 export default function HeroSection() {
-  useGSAP(() => {
-    // const titleSplit = new SplitText("#hero_title", { type: "chars, word" });
-    // gsap.from(titleSplit.chars, {
-    //   yPercent: 130,
-    //   opacity: 0,
-    //   stagger: 0.06,
-    //   duration: 1,
-    //   ease: "expo.out",
-    // });
-  }, []);
+  const heroRef = React.useRef(null);
+  const titleRef = React.useRef(null);
+  const descRef = React.useRef(null);
+
+  useGSAP(
+    () => {
+      document.querySelectorAll(".gsap-init").forEach((el) => {
+        el.classList.remove("gsap-init");
+      });
+
+      const titleSplit = new SplitText(titleRef.current, {
+        type: "chars, word",
+      });
+      titleSplit.chars.forEach((char) => {
+        char.classList.add("title-gradient-white");
+      });
+      gsap.from(titleSplit.chars, {
+        yPercent: 100,
+        duration: 0.8,
+        ease: "back.out",
+        stagger: 0.06,
+        opacity: 0,
+      });
+      gsap.from(descRef.current, {
+        scale: 0.8,
+        opacity: 0,
+        duration: 1,
+        delay: 2.2,
+        ease: "expo.out",
+      });
+    },
+    { scope: heroRef }
+  );
 
   return (
     <section
-      id="hero"
-      className="relative w-full pt-[100px] pb-40 flex bg-gray-10 bg-red-700"
+      ref={heroRef}
+      className="relative w-full pt-[100px] pb-20 flex bg-gray-10 
+      max-[640px]:pt-38 max-[400px]:pt-50"
     >
-      <div className="container flex flex-col items-center gap-40">
-        <div className="mt-20">
+      <div className="container flex flex-col items-center">
+        <div className="pb-20 pt-15">
           <h1
-            id="hero_title"
-            className="main_title title-gradient-white text-center"
+            ref={titleRef}
+            className="main_title text-center gsap-init *:inline-block *:max-[600px]:inline"
+            suppressHydrationWarning
           >
-            FRONT-END DEVELOPER & <br />
-            UI/UX DESIGNER
+            <span className="overflow-hidden max-[600px]:pr-3.5">
+              FRONT-END DEVELOPER &
+            </span>
+            <span className="overflow-hidden">UI/UX DESIGNER</span>
           </h1>
         </div>
 
-        <div className="flex flex-col items-center gap-6">
+        <div
+          className="flex flex-col items-center gap-6 gsap-init"
+          ref={descRef}
+        >
           <div className="flex gap-6">
             <div className="relative w-[98px] h-[41px]">
               <Image
