0d800ef96fe467e9745205d348ff906b21e099f0
Allan Silva <allandasilva33@outlook.com>
2026-01-12 18:24:25 -0300

    Alterar o min-height do FixedBackground

--- src/components/FixedBackground.tsx
+++ src/components/FixedBackground.tsx
@@ -3,10 +3,10 @@
 export default function FixedBackground() {
   return (
     <>
-      <div className="min-h-screen w-full bg-transparent fixed -z-10">
+      <div className="min-h-dvh w-full bg-transparent fixed -z-10">
         <div className="fixedGridGradient" />
       </div>
-      <div className="min-h-svh w-full bg-gray-9 fixed -z-20 dark:bg-gray-10">
+      <div className="min-h-dvh w-full bg-gray-9 fixed -z-20 dark:bg-gray-10">
         <div className="fixedRadialGradient" />
       </div>
     </>
